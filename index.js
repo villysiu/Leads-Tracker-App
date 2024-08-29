f6c58e26f8631dbe6cd564708697117424afdb12
Villy Siu <villysiu@gmail.com>
2024-08-29 12:25:15 -0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 
 // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 import { getDatabase,
